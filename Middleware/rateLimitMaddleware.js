de8d1f70200a83f79119056388958f0c4f2251fa
Hasan Omar <100947729+HasanOmarHasan@users.noreply.github.com>
2025-09-23 12:33:31 +0300

    add more in limiter

--- Middleware/rateLimitMaddleware.js
+++ Middleware/rateLimitMaddleware.js
@@ -5,7 +5,7 @@ const limiters = {
   // Strict limiter (1 request per minute)
   strict: rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: process.env.NODE_ENV === "dev" ? 15 : 1,
+    max: process.env.NODE_ENV === "dev" ? 15 : 2,
     message: {
       error: "Too many requests. Please wait 1 minute.",
       success: false,
@@ -17,7 +17,7 @@ const limiters = {
   // Normal limiter (100 requests per minute)
   normal: rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 50,
+    max: 60,
     message: {
       error: "Too many requests. Please try again later.",
       success: false,
@@ -29,7 +29,7 @@ const limiters = {
   // Auth limiter (5 attempts per 15 minutes)
   auth: rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 5,
+    max: 10,
     message: {
       error: "Too many authentication attempts.",
       success: false,
